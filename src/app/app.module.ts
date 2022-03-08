f84c36b1ab75fbd70940b9838374d28bf31d711f
Mohiaden Mohamoud Mohamed <muxudiinkeey@outlook.com>
2022-03-08 07:15:17 +0100

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule,
+BrowserModule,
+  FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
